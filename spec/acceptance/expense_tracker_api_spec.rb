3abe2474398bd4089e8be545a2d2673a3e578cc8
Tim Park <timsjpark@gmail.com>
2017-11-13 23:16:51 -0700

    Create pending test for getting persisted expenses

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -7,6 +7,8 @@ module ExpenseTracker
     include Rack::Test::Methods
 
     it 'records submitted expenses' do
+      pending 'Need to persist expenses'
+
       coffee = post_expense(
         'payee'  => 'Starbucks',
         'amount' => 5.75,
