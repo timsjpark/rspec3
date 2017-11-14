1126ea66458d23b19dcc8952872ed922e226bf5a
Tim Park <timsjpark@gmail.com>
2017-11-13 23:11:33 -0700

    Create test for getting expenses

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -10,8 +10,26 @@ module ExpenseTracker
       coffee = post_expense(
         'payee'  => 'Starbucks',
         'amount' => 5.75,
-        'date'   => '2017-06-20'
+        'date'   => '2017-06-10'
       )
+
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 15.25,
+        'date'   => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee'  => 'Whole Foods',
+        'amount' => 95.20,
+        'date'   => '2017-06-11'
+      )
+
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
     end
 
     def app
