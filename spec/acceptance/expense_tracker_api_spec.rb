c43f2198a1b01773af394aecf23020801f375a8a
Tim Park <timsjpark@gmail.com>
2017-11-13 23:13:49 -0700

    Create helper method for posting expenses

--- spec/acceptance/expense_tracker_api_spec.rb
+++ spec/acceptance/expense_tracker_api_spec.rb
@@ -6,23 +6,25 @@ module ExpenseTracker
   RSpec.describe 'Expense Tracker API' do
     include Rack::Test::Methods
 
+    it 'records submitted expenses' do
+      coffee = post_expense(
+        'payee'  => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2017-06-20'
+      )
+    end
+
     def app
       ExpenseTracker::API.new
     end
 
-    it 'records submitted expenses' do
-      coffee = {
-        'payee'  => 'Starbucks',
-        'amount' => 5.75,
-        'date'   => '2017-06-20'
-      }
-
-      post '/expenses', JSON.generate(coffee)
-
+    def post_expense(expense)
+      post '/expenses', JSON.generate(expense)
       expect(last_response.status).to eq(200)
 
       parsed = JSON.parse(last_response.body)
       expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
     end
   end
 end
