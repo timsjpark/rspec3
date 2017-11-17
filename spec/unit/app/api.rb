188526001dad9d82ac88dae6f29e08c759cc3d5e
Tim Park <timsjpark@gmail.com>
2017-11-16 21:43:04 -0500

    Set up some test doubles

--- spec/unit/app/api.rb
+++ spec/unit/app/api.rb
@@ -1,7 +1,18 @@
 require_relative '../../../app/api'
+require 'rack/test'
 
 module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
   RSpec.describe API do
+    include Rack::Test::Methods
+
+    def app
+      API.new(ledger: ledger)
+    end
+
+    let(:ledger) { instance_double('ExpenseTracker::Ledger') }
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
         it 'returns the expense id'
